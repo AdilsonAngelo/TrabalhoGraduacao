64106575e680953238dd7d330fbcd60e92e7bae4
AdilsonAngelo <adilson.angelo.jr@gmail.com>
2021-04-06 10:19:27 -0300

    Final countdown

--- front/fun-damentals/app.py
+++ front/fun-damentals/app.py
@@ -12,11 +12,36 @@ import stock
 import utils
 import numpy as np
 import locale
-from data.stocks import data_frame
+from data.stocks import data_frame, names, greenblatt
 from recommendations import get_top5
 from stock import top_variations
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+INITIAL_FEATURES = [
+    'VALOR DE MERCADO',
+    'DY',
+    'LPA',
+    'PSR',
+    'MARGEM BRUTA',
+    'ROE',
+]
+
+
+def to_percent_view(x): return f'{x:.2f}%'
+
+
+view_modifiers = {
+    'VALOR DE MERCADO': lambda x: locale.currency(x, grouping=True),
+    'DY': to_percent_view,
+    'MARGEM BRUTA': to_percent_view,
+    'MARGEM EBIT': to_percent_view,
+    'MARG LIQUIDA': to_percent_view,
+    'ROE': to_percent_view,
+    'ROA': to_percent_view,
+    'ROIC': to_percent_view,
+    'CAGR RECEITAS 5 ANOS': to_percent_view
+}
 
 app = dash.Dash(external_stylesheets=[dbc.themes.LUX],
                 suppress_callback_exceptions=True)
@@ -71,20 +96,51 @@ def card(title, text, color='secondary', outline=False):
 def render_stock_info(ticker: str):
 
     content = html.Div([
-        html.H2(children=[html.B(f'{ticker} - '),
-                          html.Small(f'{data_frame.loc[ticker]["SETOR"]}')]),
+        dbc.Row([
+            dbc.Col([
+                html.H2(children=[html.B(f'{ticker} - '),
+                                  html.Small(f'{names.loc[ticker]["NOME"]}')]),
+                html.Small(f'{data_frame.loc[ticker]["SETOR"]}')
+            ])
+        ]),
         html.Hr(),
         dbc.Row([
-            dbc.Col(card('Último fechamento',
+            dbc.Col(card(['Último fechamento', html.Hr()],
                          html.H2(
                              locale.currency(stock.get_last_close(
                                  ticker), grouping=True)
-                         ),
-                         'success'),
-                    )
+            ),
+                'success'),
+            ),
+            dbc.Col(card(['Preço Justo',
+                          html.Hr(),
+                          html.A(html.Small('Fórmula de Benjamin Graham'),
+                                 href='https://b7invest.com.br/2020/12/preco-justo-das-acoes-calculadora/')],
+                         html.H2(locale.currency(
+                             get_graham(data_frame.loc[ticker]), grouping=True))
+                         )
+                    ),
+            dbc.Col(
+                card(['Posição percentual',
+                      html.Hr(),
+                      html.A(html.Small('Fórmula de Joel Greenblatt'),
+                             href='https://www.btgpactualdigital.com/blog/coluna-andre-bona/a-formula-magica-de-greenblatt-para-escolha-de-acoes')],
+                     html.H2(f'{100*greenblatt.loc[ticker]:.0f}%',
+                             style={
+                                 'color': f'rgb({255*(1-greenblatt.loc[ticker])},100,{255*greenblatt.loc[ticker]})'
+                             })
+                     )
+            )
         ]),
         html.Hr(),
-        dbc.Spinner(html.Div(id="loading-output"))
+        dbc.Spinner(html.Div(id="loading-output")),
+        html.Div(id="second-output"),
+        dbc.Row(
+            dbc.Col(
+                dbc.Button("Carregar mais", id="load-more-button",
+                           color='info', className="mr-2", block=True)
+            )
+        )
     ])
 
     return content
@@ -94,19 +150,29 @@ def render_stock_info(ticker: str):
                [Input('url', 'pathname')])
 def load_rows(pathname):
     ticker = pathname.replace('/', '').upper()
-    data = crunch_data(ticker, data_frame)
+    data = crunch_data(ticker, data_frame[INITIAL_FEATURES + ['SETOR']])
+
     return gen_rows(ticker, data)
 
 
-def progress_graph(ticker: str, series: pd.Series):
+@ app.callback(Output("second-output", "children"),
+               [Input("load-more-button", "n_clicks"),
+                Input('url', 'pathname')])
+def load_more_rows(n_clicks, pathname):
+    if n_clicks == 1:
+        ticker = pathname.replace('/', '').upper()
+        data = crunch_data(ticker, data_frame[[col
+                                               for col in data_frame.columns
+                                               if col not in INITIAL_FEATURES]])
+        return gen_rows(ticker, data)
 
-    # offset = series.
-    
+
+def progress_graph(ticker: str, series: pd.Series):
     _id = ''.join(random.choices(string.ascii_lowercase, k=5))
 
     return [
         dbc.Progress(id=_id, value=100, striped=True, bar_style={
-                     'background': 'linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(0,151,255,1) 100%)'
+                     'background': 'linear-gradient(90deg, rgba(255,100,0,1) 0%, rgba(0,100,255,1) 100%)'
                      }),
         dbc.Tooltip(ticker,
                     id=f'{_id}-tt',
@@ -145,7 +211,7 @@ def simple_boxplot(ticker: str, series: pd.Series):
     return dcc.Graph(figure=fig)
 
 
-def crunch_data(ticker: str, df: pd.DataFrame):
+def crunch_data(ticker: str, df: pd.DataFrame) -> dict:
     skip = ['PRECO', 'SETOR']
 
     setor = df[df['SETOR'] == df.loc[ticker]['SETOR']]
@@ -165,10 +231,12 @@ def gen_rows(ticker: str, data: dict):
     rows = []
     row = []
     for col, val in data.items():
+        modifier = view_modifiers.get(col, lambda x: x)
+        bruto = modifier(data[col]["bruto"])
         row.append(
             dbc.Col(
                 card(
-                    [f'{col}: ',  f'{data[col]["bruto"]}'],
+                    [f'{col}: ',  f'{bruto}'],
                     [
                         html.H6('No setor'),
                         *progress_graph(ticker, data[col]['setor']),
@@ -193,15 +261,15 @@ def render_welcome():
             dbc.Spinner(html.Div(id="loading-variat"))]
 
 
-@app.callback(Output("loading-recomm", "children"),
-              [Input('url', 'pathname')])
+@ app.callback(Output("loading-recomm", "children"),
+               [Input('url', 'pathname')])
 def load_recommendations(pathname):
     return dbc.Row(dbc.Col(card(['Recomendações do mês', html.Hr(), html.Small('indicações de corretoras e analistas')],
                                 dbc.Table.from_dataframe(get_top5(), striped=True, bordered=True, hover=True))))
 
 
-@app.callback(Output("loading-variat", "children"),
-              [Input('url', 'pathname')])
+@ app.callback(Output("loading-variat", "children"),
+               [Input('url', 'pathname')])
 def load_variations(pathname):
     variations = top_variations()
     return dbc.Row([
@@ -210,6 +278,13 @@ def load_variations(pathname):
         dbc.Col(card('Maiores baixas 24h',
                      dbc.Table.from_dataframe(variations['baixas'], striped=True, bordered=True, hover=True), color='danger', outline=True))
     ])
+
+
+def get_graham(stock):
+    if stock['VPA'] < 0 or stock['LPA'] < 0:
+        return 0
+    else:
+        return (stock['VPA'] * stock['LPA'] * 22.5) ** .5
 
 
 if __name__ == '__main__':
--- front/fun-damentals/data/stocks.py
+++ front/fun-damentals/data/stocks.py
@@ -4,3 +4,12 @@ import os
 _DIR = os.path.dirname(os.path.abspath(__file__))
 
 data_frame = pd.read_csv(_DIR + '/X.csv', index_col='TICKER')
+
+names = pd.read_csv(_DIR + '/nomes.csv', index_col='TICKER')
+
+greenblatt = pd.Series([i for i in range(len(data_frame))],
+                      index=data_frame.sort_values(by=['ROE'], ascending=True).index) + \
+            pd.Series([i for i in range(len(data_frame))],
+                      index=data_frame.sort_values(by=['P/L'], ascending=False).index)
+
+greenblatt = (greenblatt - greenblatt.min()) / (greenblatt.max() - greenblatt.min())
